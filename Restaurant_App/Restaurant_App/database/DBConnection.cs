b5802bfc1db92b4fb701b79c8be23870d0131081
Juan_Diego <juan3312orrego@gmail.com>
2025-07-27 17:31:28 -0500

    Nuevo proyecto creado y configuración inicial de conexion a DB

---
+++ Restaurant_App/Restaurant_App/database/DBConnection.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+using Restaurant_App.utils;
+
+namespace Restaurant_App.database
+{
+    internal class DBConnection
+    {
+        private string server = "localhost";
+        private string database = "restaurant_db";
+        private string user = "developer";
+        private string password = "developer";
+        private string connectionString;
+        private static MySqlConnection connection = null;
+
+        public DBConnection()
+        {
+            connectionString = $"Server={server};Database={database};User ID={user};Password={password};";
+        }
+
+        public MySqlConnection GetConnection() {
+            try
+            {
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+                return connection;
+            }
+            catch (Exception ex) { 
+                MessageUtils.showErrorMessage("Error al conectar a la base de datos: " +  ex.Message);
+                return null;
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (connection != null && connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ Restaurant_App/Restaurant_App/utils/MessageUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_App.utils
+{
+    internal class MessageUtils
+    {
+        public static void showErrorMessage(string message)
+        {
+            MessageBox.Show(message, "Error", System.Windows.Forms.MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public static void showInfoMessage(string message)
+        {
+            MessageBox.Show(message, "Información", System.Windows.Forms.MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public static void showWarningMessage(string message)
+        {
+            MessageBox.Show(message, "Advertencia", System.Windows.Forms.MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
