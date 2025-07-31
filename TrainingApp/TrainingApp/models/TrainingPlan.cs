326a51b08e5ecd2675a434a3298e3dc641070cc5
Juan_Diego <juan3312orrego@gmail.com>
2025-07-30 23:35:52 -0500

    Modelos añadidos

---
+++ TrainingApp/TrainingApp/models/TrainingPlan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingApp.models
+{
+    internal class TrainingPlan
+    {
+        public long IdTrainingPlan { get; set; }
+        public string Code {  get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public long IdCompetitor { get; set; }
+        public Competitor? Competitor { get; set; }
+        public long IdSkill { get; set; }
+        public Skill? Skill { get; set; }
+
+        public TrainingPlan() { }
+
+        public TrainingPlan(long idTrainingPlan, string code, DateTime startDate, DateTime endDate, long idCompetitor, Competitor? competitor, long idSkill, Skill? skill)
+        {
+            IdTrainingPlan = idTrainingPlan;
+            Code = code;
+            StartDate = startDate;
+            EndDate = endDate;
+            IdCompetitor = idCompetitor;
+            Competitor = competitor;
+            IdSkill = idSkill;
+            Skill = skill;
+        }
+    }
+}
