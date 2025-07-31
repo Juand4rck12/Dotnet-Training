e7f6b1a90b0a29fe0682c35769373182f7c2beec
Juan_Diego <juan3312orrego@gmail.com>
2025-07-30 23:36:51 -0500

    Clase de utileria de mensajes añadida

---
+++ TrainingApp/TrainingApp/utils/MessageUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingApp.utils
+{
+    internal class MessageUtils
+    {
+        public static void ShowInfoMessage(string message)
+        {
+            MessageBox.Show(message, "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public static void ShowErrorMessage(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public static void ShowWarningMessage(string message)
+        {
+            MessageBox.Show(message, "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
